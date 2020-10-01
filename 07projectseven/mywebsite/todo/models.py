77871e71569c6aef29dea12aa10f9ff851e8b65b
Sourabh Saraswat <60272417+SourabhSaraswat@users.noreply.github.com>
2020-10-01 23:09:26 +0530

    Update models.py

--- 07projectseven/mywebsite/todo/models.py
+++ 07projectseven/mywebsite/todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Todo(models.Model):
     complete = models.BooleanField(default=False)
-    todotext = models.CharField(max_length=50)
+    todotext = models.CharField(max_length=50,null=False, blank=False)
 
     def __str__(self):
         return self.todotext
